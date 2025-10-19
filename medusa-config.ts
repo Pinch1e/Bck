46c71ee33cab3f2ef23150d24aaf6eed9daac5f9
Pinch1e <arihilamimmanuel@gmail.com>
2025-10-19 18:02:09 +0200

    Update Medusa-Config

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,16 +1,30 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+import { loadEnv, defineConfig } from "@medusajs/framework/utils"
+import fs from "fs"
+import path from "path"
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+loadEnv(process.env.NODE_ENV || "production", process.cwd())
 
-module.exports = defineConfig({
+// Load the RDS SSL certificate
+const sslCert =
+  process.env.PG_SSL_CERT ||
+  fs.readFileSync(path.join(__dirname, "rds-ca-bundle.pem")).toString()
+
+export default defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    databaseExtra: {
+      ssl: {
+        rejectUnauthorized: true,
+        ca: sslCert,
+      },
+    },
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
+      storeCors: process.env.STORE_CORS,
+      adminCors: process.env.ADMIN_CORS,
+      authCors: process.env.AUTH_CORS,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
+      port: process.env.PORT || 9000,
+    },
+  },
 })
