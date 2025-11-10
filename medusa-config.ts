235461607984b5779f3bccbe61deb35a7b103c2c
Pinch1e <arihilamimmanuel@gmail.com>
2025-11-10 12:32:46 +0100

    Some Addition

--- medusa-config.ts
+++ medusa-config.ts
@@ -20,16 +20,15 @@ export default defineConfig({
     },
   },
 
-  modules: [
-    {
+  modules: {
+    file: {
       resolve: "@medusajs/file-s3",
       options: {
-        s3_url: process.env.S3_URL || "https://s3.amazonaws.com",
         access_key_id: process.env.S3_ACCESS_KEY_ID,
         secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
         region: process.env.S3_REGION || "eu-central-1",
         bucket: process.env.S3_BUCKET_NAME,
       },
     },
-  ],
+  },
 })
